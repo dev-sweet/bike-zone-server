d7379994c7f639b3096ae7ea6d900fda1444e043
dev-sweet <sweetali0520@gmail.com>
2022-02-21 16:28:16 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-async function run() {}
-run().catch(console.log(dir));
+// mongo uri and client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mas8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // connect with mongodb
+    await client.connect();
+    const database = client.db("bike-zone");
+    const bikeCollection = database.collection("bikes");
+
+    app.get("/bikes", async (req, res) => {
+      const cursor = bikeCollection.find({});
+      const bikes = await cursor.toArray();
+      res.json(bikes);
+    });
+  } finally {
+    // close connection
+    // client.close();
+  }
+}
+run().catch(console.dir());
 
 app.get("/", (req, res) => {
-  res.send("bike zone server is ready");
+  res.send("bike zone server is running");
+  console.log("bike zone server is running");
 });
 app.listen(port, () => {
   console.log(`bike zone server running at port ${port}`);
